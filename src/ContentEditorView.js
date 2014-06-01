7a2ab1a0f1ff156ff254b43519bb4c2d27610725
LeeJinKwon <lee.jinkwon@nhn.com>
2014-06-01 14:44:14 +0900

    Develop New Prototype

--- src/ContentEditorView.js
+++ src/ContentEditorView.js
@@ -3,40 +3,127 @@ var ContentEditorView = Backbone.View.extend({
 
         this.setEvents();
     },
-    setEvents : function(){
-        // This code is generally not necessary, but it is here to demonstrate
+    resize : function(){
+        setTimeout(function(){
+            $('#_content_editor').css('height', '');
+            var nHeight = $('#_content_editor').height();
+            $('#_content_editor').css('height', nHeight + 'px');
+
+        }, 50);
+    },
+
+    initCKEditor : function (){
+// This code is generally not necessary, but it is here to demonstrate
         // how to customize specific editor instances on the fly. This fits well
         // this demo because we have editable elements (like headers) that
         // require less features.
 
         // The "instanceCreated" event is fired for every editor instance created.
-        CKEDITOR.on( 'instanceCreated', function( event ) {
+        CKEDITOR.on('instanceCreated', function (event){
             var editor = event.editor,
                 element = editor.element;
 
             // Customize editors for headers and tag list.
             // These editors don't need features like smileys, templates, iframes etc.
-                // Customize the editor configurations on "configLoaded" event,
-                // which is fired after the configuration file loading and
-                // execution. This makes it possible to change the
-                // configurations before the editor initialization takes place.
-                editor.on( 'configLoaded', function() {
+            // Customize the editor configurations on "configLoaded" event,
+            // which is fired after the configuration file loading and
+            // execution. This makes it possible to change the
+            // configurations before the editor initialization takes place.
+            editor.on('configLoaded', function (){
 
-                    // Remove unnecessary plugins to make the editor simpler.
-                    editor.config.removePlugins = 'colorbutton,find,flash,font,' +
-                        'forms,iframe,image,newpage,removeformat,' +
-                        'smiley,specialchar,stylescombo,templates';
+                // Remove unnecessary plugins to make the editor simpler.
+                editor.config.removePlugins = 'colorbutton,find,flash,font,' +
+                    'forms,iframe,image,newpage,removeformat,' +
+                    'smiley,specialchar,stylescombo,templates';
 
-                    // Rearrange the layout of the toolbar.
-                    editor.config.toolbarGroups = [
-                        { name: 'editing',		groups: [ 'basicstyles', 'links' ] }
+                // Rearrange the layout of the toolbar.
+                editor.config.toolbarGroups = [
+                    { name : 'editing', groups : [ 'basicstyles', 'links' ] }
 //                        { name: 'undo' },
 //                        { name: 'clipboard',	groups: [ 'selection', 'clipboard' ] },
 //                        { name: 'about' }
-                    ];
-                });
+                ];
+            });
+        });
+    }, setEvents : function(){
+
+        var current;
+
+        function SetToBold (e) {
+            $(current).attr('contenteditable', 'true');
+            document.execCommand('bold', false, true);
+
+            $(current).contents().focus();
+            return false;
+        }
+
+        function resize(){
+            setTimeout(function(){
+                $('#_content_editor').css('height', '');
+                var nHeight = $('#_content_editor').height();
+                $('#_content_editor').css('height', nHeight + 'px');
+
+            }, 50);
+        }
+
+
+        $('#_bold').on('click', function(){
+            SetToBold();
         });
 
+        $(document).on('focus', "div[contenteditable=true]", function() {
+            current = this;
+        });
+
+        $(document)// make sure br is always the lastChild of contenteditable
+            .on("keydown keyup", 'div[contenteditable=true]', function(e){
+
+                resize();
+                if(e.which === 9) {
+                    e.preventDefault();
+                } else if(e.which === 27){
+
+                    $(e.currentTarget).trigger('blur');
+                    return;
+
+                } else if (e.which == 13) {
+                    if (window.getSelection) {
+                        var selection = window.getSelection(),
+                            range = selection.getRangeAt(0),
+                            br = document.createElement("br");
+                        range.deleteContents();
+                        range.insertNode(br);
+                        range.setStartAfter(br);
+                        range.setEndAfter(br);
+                        range.collapse(false);
+                        selection.removeAllRanges();
+                        selection.addRange(range);
+                        return false;
+                    }
+
+                }
+
+
+            });
+
+        $('#_add_img').on('click', function(){
+            $('#_content_editor').append($('#_img_tmpl').html());
+
+            resize();
+        });
+
+        $('#_add_movie').on('click', function(){
+            $('#_content_editor').append($('#_movie_tmpl').html());
+            resize();
+        });
+
+        $('#_add_text').on('click', function(){
+
+            $('#_append_item').trigger('click');
+        });
+
+
+        this.initCKEditor();
 
         this.$el.on('blur', 'div', $.proxy(function(e){
 
@@ -53,9 +140,11 @@ var ContentEditorView = Backbone.View.extend({
                 $(e.currentTarget).remove();
             }
 
-            this.editor.destroy();
+            if(this.editor){
+                this.editor.destroy();
+                this.editor = null;
+            }
 
-            this.editor = null;
             this.$el.sortable("option", 'disabled', false);
         }, this));
 
@@ -63,7 +152,7 @@ var ContentEditorView = Backbone.View.extend({
 
             var target = $(e.currentTarget).parent();
 
-            if(this.editor !== null){
+            if(this.editor){
                 this.editor.destroy();
                 this.editor = null;
             }
@@ -72,6 +161,7 @@ var ContentEditorView = Backbone.View.extend({
                 target.remove();
                 this.$el.sortable("option", 'disabled', false);
 
+                this.resize();
             }, this));
 
             e.stopPropagation();
@@ -80,11 +170,7 @@ var ContentEditorView = Backbone.View.extend({
         }, this));
 
         this.$el.on('mouseenter', 'div', $.proxy(function(e){
-
-            if(this.editor !== null){ return false;}
-
             $(e.currentTarget).addClass('hover');
-
             $(e.currentTarget).find('._remove').remove();
             $(e.currentTarget).prepend('<span class="_remove remove_item"></span>');
 
@@ -102,7 +188,7 @@ var ContentEditorView = Backbone.View.extend({
 
             $('#__cke').find('._remove').remove();
 
-            this.$el.append('<div id="__cke" class="_text" style="outline:none;"></div>');
+            this.$el.append('<div id="__cke" class="_text" style="clear:both;outline:none;"></div>');
 
             this.editor = CKEDITOR.inline('__cke');
 
@@ -116,13 +202,14 @@ var ContentEditorView = Backbone.View.extend({
             this.$el.sortable("option", 'disabled', true).enableSelection();
 
 
+
         }, this));
 
         this.$el.on('click', 'div', $.proxy(function(e){
 
             if(
                 $(e.currentTarget).attr('id') === '__cke' ||
-                $(e.currentTarget).hasClass('_text') === false ||
+//                $(e.currentTarget).hasClass('_text') === false ||
                 this.bSort === true
             ){
                 return;
@@ -137,7 +224,7 @@ var ContentEditorView = Backbone.View.extend({
                 .attr('id', '__cke')
                 .focus();
 
-            this.editor = CKEDITOR.inline('__cke');
+//            this.editor = CKEDITOR.inline('__cke');
 
             this.targetElement = $(e.currentTarget);
 
@@ -145,11 +232,16 @@ var ContentEditorView = Backbone.View.extend({
         }, this));
 
 
+
         this.$el.sortable({
             delay : 250,
+            axis : 'y',
             tolerance: 'pointer',
             revert: 250,
+            containment: "parent",
+            cursor: "move",
             start: $.proxy(function(e, ui ){
+
                 this.bSort = true;
                 ui.placeholder.height(ui.helper.height());
                 ui.helper.addClass('grap');
